93f1ea990bdeba4be8d13ec5a03bd6d03a62dbf3
David Li <a474247132@gmail.com>
2019-01-29 15:19:23 -0500

    hint button first draft

--- src/InteractivePage/InteractivePage.js
+++ src/InteractivePage/InteractivePage.js
@@ -9,15 +9,34 @@ import {ReactComponent as Circle} from '../assets/circle.svg';
 class InteractivePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      'Clicked': false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({'Clicked': true});
   }
 
   render() {
+    let hintClassName = '';
+    if (!this.state.Clicked) {
+      hintClassName = ' svg-hint-button-unclicked';
+    }
     return (
       <div className="interactive-page">
         {'Interface ' + (store.getState().currentPage + 1)}
         <div className='hint-button'>
-        	<Circle className='svg-hint-button'/>
+        	<Circle className={'svg-hint-button' + hintClassName}
+                  onClick={this.handleClick}/>
         </div>
+        {!this.state.Clicked || 
+          <div className='text-bar'>
+            Button Clicked
+          </div>
+        }
       </div>
     );
   }
