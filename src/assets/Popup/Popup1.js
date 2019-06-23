822f89388af982a16e8d79ce27f90c851b069808
Yang Li <yang.li@YLMACBSN000642.local>
2019-06-23 11:18:35 -0700

    popup image example

--- src/assets/Popup/Popup1.js
+++ src/assets/Popup/Popup1.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './Popup1.css';
 
+import Image1 from '../Module 0_V01/Module01-01.jpg';
+
 export default function (props) {
 	return (
 		<div className='popup-class-1'>
 			<div className='popup-font-small'>
 				Hello this is popup 1 small font
 			</div>
+			<img className='popup-image' src={Image1} />
 			<div className='popup-font-big'>
 				Hello this is popup 1 big font
 			</div>
