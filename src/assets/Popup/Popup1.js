4955cdcd1ea58f710607d17d5f376fee9b92a3fe
Yang Li <yang.li@YLMACBSN000642.local>
2019-06-23 10:39:52 -0700

    add css for popup html

--- src/assets/Popup/Popup1.js
+++ src/assets/Popup/Popup1.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './Popup1.css';
+
 export default function (props) {
 	return (
-		<div>
-			Hello this is popup 1
+		<div className='popup-class-1'>
+			<div className='popup-font-small'>
+				Hello this is popup 1 small font
+			</div>
+			<div className='popup-font-big'>
+				Hello this is popup 1 big font
+			</div>
 		</div> 
 	);
 }
