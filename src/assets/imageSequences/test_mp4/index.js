5ccc21bd5cbb4b9ed7c6dd90a435b8f71002cff4
刘悦 <Nino@liuyuedeMacBook-Pro.local>
2019-07-02 12:50:52 -0400

    shawn

--- src/assets/imageSequences/test_mp4/index.js
+++ src/assets/imageSequences/test_mp4/index.js
@@ -1,33 +1,208 @@
-import Image1 from './finalllllllllllllllllllllllrender_00000.png';
-import Image2 from './finalllllllllllllllllllllllrender_00001.png';
-import Image3 from './finalllllllllllllllllllllllrender_00002.png';
-import Image4 from './finalllllllllllllllllllllllrender_00003.png';
-import Image5 from './finalllllllllllllllllllllllrender_00004.png';
-import Image6 from './finalllllllllllllllllllllllrender_00005.png';
-import Image7 from './finalllllllllllllllllllllllrender_00006.png';
-import Image8 from './finalllllllllllllllllllllllrender_00007.png';
-import Image9 from './finalllllllllllllllllllllllrender_00008.png';
-import Image10 from './img0010.png';
-import Image11 from './img0011.png';
-import Image12 from './img0012.png';
-import Image13 from './img0013.png';
-import Image14 from './img0014.png';
-import Image15 from './img0015.png';
-import Image16 from './img0016.png';
-import Image17 from './img0017.png';
-import Image18 from './img0018.png';
-import Image19 from './img0019.png';
-import Image20 from './img0020.png';
-import Image21 from './img0021.png';
-import Image22 from './img0022.png';
-import Image23 from './img0023.png';
-import Image24 from './img0024.png';
-import Image25 from './img0025.png';
-import Image26 from './img0026.png';
-import Image27 from './img0027.png';
-import Image28 from './img0028.png';
-import Image29 from './img0029.png';
+import Image1 from './finalrender/Renderseq_00000.png';
+import Image2 from './finalrender/Renderseq_00001.png';
+import Image3 from './finalrender/Renderseq_00002.png';
+import Image4 from './finalrender/Renderseq_00003.png';
+import Image5 from './finalrender/Renderseq_00004.png';
+import Image6 from './finalrender/Renderseq_00005.png';
+import Image7 from './finalrender/Renderseq_00006.png';
+import Image8 from './finalrender/Renderseq_00007.png';
+import Image9 from './finalrender/Renderseq_00008.png';
+import Image10 from './finalrender/Renderseq_00009.png';
+import Image11 from './finalrender/Renderseq_00010.png';
+import Image12 from './finalrender/Renderseq_00011.png';
+import Image13 from './finalrender/Renderseq_00012.png';
+import Image14 from './finalrender/Renderseq_00013.png';
+import Image15 from './finalrender/Renderseq_00014.png';
+import Image16 from './finalrender/Renderseq_00015.png';
+import Image17 from './finalrender/Renderseq_00016.png';
+import Image18 from './finalrender/Renderseq_00017.png';
+import Image19 from './finalrender/Renderseq_00018.png';
+import Image20 from './finalrender/Renderseq_00019.png';
+import Image21 from './finalrender/Renderseq_00020.png';
+import Image22 from './finalrender/Renderseq_00021.png';
+import Image23 from './finalrender/Renderseq_00022.png';
+import Image24 from './finalrender/Renderseq_00023.png';
+import Image25 from './finalrender/Renderseq_00024.png';
+import Image26 from './finalrender/Renderseq_00025.png';
+import Image27 from './finalrender/Renderseq_00026.png';
+import Image28 from './finalrender/Renderseq_00027.png';
+import Image29 from './finalrender/Renderseq_00028.png';
+import Image30 from './finalrender/Renderseq_00029.png';
+import Image31 from './finalrender/Renderseq_00030.png';
+import Image32 from './finalrender/Renderseq_00031.png';
+import Image33 from './finalrender/Renderseq_00032.png';
+import Image34 from './finalrender/Renderseq_00033.png';
+import Image35 from './finalrender/Renderseq_00034.png';
+import Image36 from './finalrender/Renderseq_00035.png';
+import Image37 from './finalrender/Renderseq_00036.png';
+import Image38 from './finalrender/Renderseq_00037.png';
+import Image39 from './finalrender/Renderseq_00038.png';
+import Image40 from './finalrender/Renderseq_00039.png';
+import Image41 from './finalrender/Renderseq_00040.png';
+import Image42 from './finalrender/Renderseq_00041.png';
+import Image43 from './finalrender/Renderseq_00042.png';
+import Image44 from './finalrender/Renderseq_00043.png';
+import Image45 from './finalrender/Renderseq_00044.png';
+import Image46 from './finalrender/Renderseq_00045.png';
+import Image47 from './finalrender/Renderseq_00046.png';
+import Image48 from './finalrender/Renderseq_00047.png';
+import Image49 from './finalrender/Renderseq_00048.png';
+import Image50 from './finalrender/Renderseq_00049.png';
+import Image51 from './finalrender/Renderseq_00050.png';
+import Image52 from './finalrender/Renderseq_00051.png';
+import Image53 from './finalrender/Renderseq_00052.png';
+import Image54 from './finalrender/Renderseq_00053.png';
+import Image55 from './finalrender/Renderseq_00054.png';
+import Image56 from './finalrender/Renderseq_00055.png';
+import Image57 from './finalrender/Renderseq_00056.png';
+import Image58 from './finalrender/Renderseq_00057.png';
+import Image59 from './finalrender/Renderseq_00058.png';
+import Image60 from './finalrender/Renderseq_00059.png';
+import Image61 from './finalrender/Renderseq_00060.png';
+import Image62 from './finalrender/Renderseq_00061.png';
+import Image63 from './finalrender/Renderseq_00062.png';
+import Image64 from './finalrender/Renderseq_00063.png';
+import Image65 from './finalrender/Renderseq_00064.png';
+import Image66 from './finalrender/Renderseq_00065.png';
+import Image67 from './finalrender/Renderseq_00066.png';
+import Image68 from './finalrender/Renderseq_00067.png';
+import Image69 from './finalrender/Renderseq_00068.png';
+import Image70 from './finalrender/Renderseq_00069.png';
+import Image71 from './finalrender/Renderseq_00070.png';
+import Image72 from './finalrender/Renderseq_00071.png';
+import Image73 from './finalrender/Renderseq_00072.png';
+import Image74 from './finalrender/Renderseq_00073.png';
+import Image75 from './finalrender/Renderseq_00074.png';
+import Image76 from './finalrender/Renderseq_00075.png';
+import Image77 from './finalrender/Renderseq_00076.png';
+import Image78 from './finalrender/Renderseq_00077.png';
+import Image79 from './finalrender/Renderseq_00078.png';
+import Image80 from './finalrender/Renderseq_00079.png';
+import Image81 from './finalrender/Renderseq_00080.png';
+import Image82 from './finalrender/Renderseq_00081.png';
+import Image83 from './finalrender/Renderseq_00082.png';
+import Image84 from './finalrender/Renderseq_00083.png';
+import Image85 from './finalrender/Renderseq_00084.png';
+import Image86 from './finalrender/Renderseq_00085.png';
+import Image87 from './finalrender/Renderseq_00086.png';
+import Image88 from './finalrender/Renderseq_00087.png';
+import Image89 from './finalrender/Renderseq_00088.png';
+import Image90 from './finalrender/Renderseq_00089.png';
+import Image91 from './finalrender/Renderseq_00090.png';
+import Image92 from './finalrender/Renderseq_00091.png';
+import Image93 from './finalrender/Renderseq_00092.png';
+import Image94 from './finalrender/Renderseq_00093.png';
+import Image95 from './finalrender/Renderseq_00094.png';
+import Image96 from './finalrender/Renderseq_00095.png';
+import Image97 from './finalrender/Renderseq_00096.png';
+import Image98 from './finalrender/Renderseq_00097.png';
+import Image99 from './finalrender/Renderseq_00098.png';
+import Image100 from './finalrender/Renderseq_00099.png';
+import Image101 from './finalrender/Renderseq_00100.png';
+import Image102 from './finalrender/Renderseq_00101.png';
+import Image103 from './finalrender/Renderseq_00102.png';
+import Image104 from './finalrender/Renderseq_00103.png';
+import Image105 from './finalrender/Renderseq_00104.png';
+import Image106 from './finalrender/Renderseq_00105.png';
+import Image107 from './finalrender/Renderseq_00106.png';
+import Image108 from './finalrender/Renderseq_00107.png';
+import Image109 from './finalrender/Renderseq_00108.png';
+import Image110 from './finalrender/Renderseq_00109.png';
+import Image111 from './finalrender/Renderseq_00110.png';
+import Image112 from './finalrender/Renderseq_00111.png';
+import Image113 from './finalrender/Renderseq_00112.png';
+import Image114 from './finalrender/Renderseq_00113.png';
+import Image115 from './finalrender/Renderseq_00114.png';
+import Image116 from './finalrender/Renderseq_00115.png';
+import Image117 from './finalrender/Renderseq_00116.png';
+import Image118 from './finalrender/Renderseq_00117.png';
+import Image119 from './finalrender/Renderseq_00118.png';
+import Image120 from './finalrender/Renderseq_00119.png';
+import Image121 from './finalrender/Renderseq_00120.png';
+import Image122 from './finalrender/Renderseq_00121.png';
+import Image123 from './finalrender/Renderseq_00122.png';
+import Image124 from './finalrender/Renderseq_00123.png';
+import Image125 from './finalrender/Renderseq_00124.png';
+import Image126 from './finalrender/Renderseq_00125.png';
+import Image127 from './finalrender/Renderseq_00126.png';
+import Image128 from './finalrender/Renderseq_00127.png';
+import Image129 from './finalrender/Renderseq_00128.png';
+import Image130 from './finalrender/Renderseq_00129.png';
+import Image131 from './finalrender/Renderseq_00130.png';
+import Image132 from './finalrender/Renderseq_00131.png';
+import Image133 from './finalrender/Renderseq_00132.png';
+import Image134 from './finalrender/Renderseq_00133.png';
+import Image135 from './finalrender/Renderseq_00134.png';
+import Image136 from './finalrender/Renderseq_00135.png';
+import Image137 from './finalrender/Renderseq_00136.png';
+import Image138 from './finalrender/Renderseq_00137.png';
+import Image139 from './finalrender/Renderseq_00138.png';
+import Image140 from './finalrender/Renderseq_00139.png';
+import Image141 from './finalrender/Renderseq_00140.png';
+import Image142 from './finalrender/Renderseq_00141.png';
+import Image143 from './finalrender/Renderseq_00142.png';
+import Image144 from './finalrender/Renderseq_00143.png';
+import Image145 from './finalrender/Renderseq_00144.png';
+import Image146 from './finalrender/Renderseq_00145.png';
+import Image147 from './finalrender/Renderseq_00146.png';
+import Image148 from './finalrender/Renderseq_00147.png';
+import Image149 from './finalrender/Renderseq_00148.png';
+import Image150 from './finalrender/Renderseq_00149.png';
+import Image151 from './finalrender/Renderseq_00150.png';
+import Image152 from './finalrender/Renderseq_00151.png';
+import Image153 from './finalrender/Renderseq_00152.png';
+import Image154 from './finalrender/Renderseq_00153.png';
+import Image155 from './finalrender/Renderseq_00154.png';
+import Image156 from './finalrender/Renderseq_00155.png';
+import Image157 from './finalrender/Renderseq_00156.png';
+import Image158 from './finalrender/Renderseq_00157.png';
+import Image159 from './finalrender/Renderseq_00158.png';
+import Image160 from './finalrender/Renderseq_00159.png';
+import Image161 from './finalrender/Renderseq_00160.png';
+import Image162 from './finalrender/Renderseq_00161.png';
+import Image163 from './finalrender/Renderseq_00162.png';
+import Image164 from './finalrender/Renderseq_00163.png';
+import Image165 from './finalrender/Renderseq_00164.png';
+import Image166 from './finalrender/Renderseq_00165.png';
+import Image167 from './finalrender/Renderseq_00166.png';
+import Image168 from './finalrender/Renderseq_00167.png';
+import Image169 from './finalrender/Renderseq_00168.png';
+import Image170 from './finalrender/Renderseq_00169.png';
+import Image171 from './finalrender/Renderseq_00170.png';
+import Image172 from './finalrender/Renderseq_00171.png';
+import Image173 from './finalrender/Renderseq_00172.png';
+import Image174 from './finalrender/Renderseq_00173.png';
+import Image175 from './finalrender/Renderseq_00174.png';
+import Image176 from './finalrender/Renderseq_00175.png';
+import Image177 from './finalrender/Renderseq_00176.png';
+import Image178 from './finalrender/Renderseq_00177.png';
+import Image179 from './finalrender/Renderseq_00178.png';
+import Image180 from './finalrender/Renderseq_00179.png';
+import Image181 from './finalrender/Renderseq_00180.png';
+import Image182 from './finalrender/Renderseq_00181.png';
+import Image183 from './finalrender/Renderseq_00182.png';
+import Image184 from './finalrender/Renderseq_00183.png';
+import Image185 from './finalrender/Renderseq_00184.png';
+import Image186 from './finalrender/Renderseq_00185.png';
+import Image187 from './finalrender/Renderseq_00186.png';
+import Image188 from './finalrender/Renderseq_00187.png';
+import Image189 from './finalrender/Renderseq_00188.png';
+import Image190 from './finalrender/Renderseq_00189.png';
+import Image191 from './finalrender/Renderseq_00190.png';
+import Image192 from './finalrender/Renderseq_00191.png';
+import Image193 from './finalrender/Renderseq_00192.png';
+import Image194 from './finalrender/Renderseq_00193.png';
+import Image195 from './finalrender/Renderseq_00194.png';
+import Image196 from './finalrender/Renderseq_00195.png';
+import Image197 from './finalrender/Renderseq_00196.png';
+import Image198 from './finalrender/Renderseq_00197.png';
+import Image199 from './finalrender/Renderseq_00198.png';
+import Image200 from './finalrender/Renderseq_00199.png';
+import Image201 from './finalrender/Renderseq_00200.png';
 
+
+
+//import Image from './finalrender/Renderseq_000.png';
 export const imageSequence = [
 	Image1,
 	Image2,
@@ -58,4 +233,178 @@ export const imageSequence = [
 	Image27,
 	Image28,
 	Image29,
+	Image30,
+	Image31,
+	Image32,
+	Image33,
+	Image34,
+	Image35,
+	Image36,
+	Image37,
+	Image38,
+	Image39,
+	Image40,
+	Image41,
+	Image42,
+	Image43,
+	Image44,
+	Image45,
+	Image46,
+	Image47,
+	Image48,
+	Image49,
+	Image50,
+	Image51,
+	Image52,
+	Image53,
+	Image54,
+	Image55,
+	Image56,
+	Image57,
+    Image58,
+	Image59,
+	Image60,
+    Image61,
+    Image62,
+    Image63,
+    Image64,
+    Image65,
+    Image66,
+    Image67,
+    Image68,
+    Image69,
+    Image70,
+    Image71,
+    Image72,
+    Image73,
+    Image74,
+    Image75,
+    Image76,
+    Image77,
+    Image78,
+    Image79,
+    Image80,
+    Image81,
+    Image82,
+    Image83,
+    Image84,
+    Image85,
+    Image86,
+    Image87,
+    Image88,
+    Image89,
+    Image90,
+    Image91,
+    Image92,
+    Image93,
+    Image94,
+    Image95,
+    Image96,
+    Image97,
+    Image98,
+    Image99,
+    Image100,
+    Image101,
+    Image102,
+    Image103,
+    Image104,
+    Image105,
+    Image106,
+    Image107,
+    Image108,
+    Image109,
+    Image110,
+    Image111,
+    Image112,
+    Image113,
+    Image114,
+    Image115,
+    Image116,
+    Image117,
+    Image118,
+    Image119,
+    Image120,
+    Image121,
+    Image122,
+    Image123,
+    Image124,
+    Image125,
+    Image126,
+    Image127,
+    Image128,
+    Image129,
+    Image130,
+    Image131,
+    Image132,
+    Image133,
+    Image134,
+    Image135,
+    Image136,
+    Image137,
+    Image138,
+    Image139,
+    Image140,
+    Image141,
+    Image142,
+    Image143,
+    Image144,
+    Image145,
+    Image146,
+    Image147,
+    Image148,
+    Image149,
+    Image150,
+    Image151,
+    Image152,
+    Image153,
+    Image154,
+    Image155,
+    Image156,
+    Image157,
+    Image158,
+    Image159,
+    Image160,
+    Image161,
+    Image162,
+    Image163,
+    Image164,
+    Image165,
+    Image166,
+    Image167,
+    Image168,
+    Image169,
+    Image170,
+    Image171,
+    Image172,
+    Image173,
+    Image174,
+    Image175,
+    Image176,
+    Image177,
+    Image178,
+    Image179,
+    Image180,
+    Image181,
+    Image182,
+    Image183,
+    Image184,
+    Image185,
+    Image186,
+    Image187,
+    Image188,
+    Image189,
+    Image190,
+    Image191,
+    Image192,
+    Image193,
+    Image194,
+    Image195,
+    Image196,
+    Image197,
+    Image198,
+    Image199,
+    Image200,
+    Image201,
+	//Image,
+
 ];
